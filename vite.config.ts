63aac8954a2b97c02f902fddb178193be1c8b2a9
theshenron <gourav143faz@gmail.com>
2025-11-06 01:41:34 +0530

    init & vitest

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/setupTests.ts',
+//   },
+// })
+
+
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+})
