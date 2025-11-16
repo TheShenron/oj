29c0c8c2c15ab6000f3cbb08eebedda05bc44d33
theshenron <gourav143faz@gmail.com>
2025-11-17 03:21:35 +0530

    File upload added

--- nextwebapp/src/app/(protected)/dashboard/page.tsx
+++ nextwebapp/src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import FileUpload01 from "@/components/file-upload-01"
 import { AppSidebar } from "@/components/sidebar/app-sidebar"
 import {
   Breadcrumb,
@@ -42,12 +43,13 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <FileUpload01 />
+          {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="bg-muted/50 aspect-video rounded-xl" />
             <div className="bg-muted/50 aspect-video rounded-xl" />
             <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          </div> */}
+          {/* <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" /> */}
         </div>
       </SidebarInset>
     </SidebarProvider>
