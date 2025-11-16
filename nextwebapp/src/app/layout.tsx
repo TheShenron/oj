2bb3ca89639171479e9a2ec14de3761c0f20fb9e
theshenron <gourav143faz@gmail.com>
2025-11-16 05:46:30 +0530

    shadcn added

--- nextwebapp/src/app/layout.tsx
+++ nextwebapp/src/app/layout.tsx
@@ -1,14 +1,13 @@
 // app/layout.tsx
-
-export const metadata = {
-    title: "My App",
-};
+import { Button } from '@/components/ui/button';
+import '@/styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
                 {children}
+                <Button>Click Me</Button>
             </body>
         </html>
     );
