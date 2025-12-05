a0713fdecc34e7d618c28317326900e31aa3e138
theshenron <gourav143faz@gmail.com>
2025-12-06 02:04:41 +0530

    working on github login

--- nextwebapp/src/app/(public)/login/components/LoginForm.tsx
+++ nextwebapp/src/app/(public)/login/components/LoginForm.tsx
@@ -4,28 +4,17 @@
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-//form
-import * as z from "zod";
-import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { loginSchema } from "@/app/(public)/login/components/LoginSchema";
-import { login } from "@/lib/services/auth";
-
 //icons
 import { GalleryVerticalEnd } from "lucide-react";
 
 //UI components
 import { cn } from "@/lib/utils";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Field,
   FieldDescription,
-  FieldError,
   FieldGroup,
-  FieldLabel,
 } from "@/components/ui/field";
-import { toast } from "sonner";
 
 export function LoginComp({
   className,
@@ -33,75 +22,35 @@ export function LoginComp({
 }: React.ComponentProps<"div">) {
   const router = useRouter();
 
-  const { control, handleSubmit } = useForm<z.infer<typeof loginSchema>>({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      magicURL: "",
-    },
-  });
-
-  const onSubmit = async (data: z.infer<typeof loginSchema>) => {
-    const toastId = toast.loading("Logging in...");
-
-    try {
-      const res = await login({ magicURL: data.magicURL });
-      console.log(res, 'login res');
-      toast.success("Login successful!", { id: toastId });
-      router.replace("/evaluation");
-    } catch (error) {
-      console.log(error, 'login error');
-      toast.error("Login failed", { id: toastId });
-    }
-
-  };
-
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FieldGroup>
-          <div className="flex flex-col items-center gap-2 text-center">
-            <Link
-              href="#"
-              className="flex flex-col items-center gap-2 font-medium"
-            >
-              <div className="flex size-8 items-center justify-center rounded-md">
-                <GalleryVerticalEnd className="size-6" />
-              </div>
-            </Link>
-            <h1 className="text-xl font-bold">Welcome to Nikonerds Inc.</h1>
-            <FieldDescription>
-              Don&apos;t have an account? <Link href="/signup">Sign up</Link>
-            </FieldDescription>
-          </div>
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-2 text-center">
+          <Link
+            href="#"
+            className="flex flex-col items-center gap-2 font-medium"
+          >
+            <div className="flex size-8 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-6" />
+            </div>
+          </Link>
+          <h1 className="text-xl font-bold">Welcome to Nikonerds Inc.</h1>
+        </div>
 
-          <Controller
-            name="magicURL"
-            control={control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor="magicURL">Magic URL</FieldLabel>
-                <Input
-                  {...field}
-                  id="magicURL"
-                  type="magicURL"
-                  required
-                  placeholder="demo@example.com"
-                  aria-invalid={fieldState.invalid}
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
-          />
-
-          <Field>
-            <Button type="submit">Verify</Button>
-          </Field>
-        </FieldGroup>
-      </form>
+        <Field>
+          <Button variant="outline" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                fill="currentColor"
+              />
+            </svg>
+            Login with GitHub
+          </Button>
+        </Field>
+      </FieldGroup>
       <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our{" "}
+        By clicking login, you agree to our{" "}
         <Link href="#">Terms of Service</Link> and{" "}
         <Link href="#">Privacy Policy</Link>.
       </FieldDescription>
--- nextwebapp/src/app/(public)/login/components/LoginSchema.ts
+++
@@ -1,7 +0,0 @@
-import z from "zod";
-
-export const loginSchema = z.object({
-  magicURL: z
-    .string("Please enter a Magic URL")
-    .min(5, "Magic URL must be at least 5 characters long"),
-});
